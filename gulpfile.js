ec774cf16831aab57318843c94e83e443260746b
pablo goris <goris00@gmail.com>
2016-03-08 21:53:40 +0100

    add more node modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 var uglify = require("gulp-uglify");
 var minifyCss = require("gulp-minify-css");
-var browserSync = require('browser-sync').create();
 
 // css
 gulp.task('sass', function () {
@@ -19,18 +18,10 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('dist/js'));
 
 });
-// Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "http://localhost/web/uoc-project/uoc-project-2/"
-        }
-    });
-});
 // watching tasks
 gulp.task('watch', function () {
     gulp.watch('src/js/*.js', ['scripts']);
     gulp.watch('src/sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['scripts', 'sass', 'browser-sync', 'watch']);
+gulp.task('default', ['scripts', 'sass', 'watch']);
